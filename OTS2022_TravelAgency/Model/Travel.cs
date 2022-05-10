73509e3c64154cb394808e0eaa407d7da05dfc78
Andrej Katin <katin.andrej96@gmail.com>
2022-05-10 22:45:47 +0200

    FC

---
+++ OTS2022_TravelAgency/Model/Travel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTS2022_TravelAgency.Model
+{
+    public class Travel
+    {
+        public TransportType ReserveTransport(string destination, int numberOfDays, bool extraLuggage, bool mealIncluded)
+        {
+            return 0;
+        }
+    }
+}
